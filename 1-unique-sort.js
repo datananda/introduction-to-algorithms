293b592ac4f97a1e4ac0dce97ac1bdce9ca285a1
Madeleine Filloux <mfilloux@gmail.com>
2020-03-27 18:55:48 -0700

    add solutions to exercises 1 and 2

---
+++ 1-unique-sort.js
@@ -0,0 +1,21 @@
+// Task: Transform this simple sorting algorithm into a unique sort. 
+// It should not return any duplicate values in the sorted array.
+
+// input: [1,5,2,1] => output: [1,2,5]
+// input: [4,2,2,3,2,2,2] => output: [2,3,4]
+
+const uniqSort = function(arr) {
+    const breadcrumbs = {};
+    const uniqueArray = [];
+
+    arr.forEach(elem => {
+        if (!breadcrumbs[elem]) {
+            uniqueArray.push(elem);
+            breadcrumbs[elem] = true;
+        }
+    });
+    
+    return uniqueArray.sort((a, b) => a - b);
+};
+
+console.log(uniqSort([4,2,2,3,2,2,2])); // => [2,3,4]
